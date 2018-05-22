a0a5ae8d8bfc36de125278f7563ef80111adbffa
dinghuanghao <aa531811820@gmail.com>
2018-05-22 14:43:51 +0800

    修改cnn.py名字为baseline.py

--- model/baseline1/baseline.py
+++ model/baseline1/baseline.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 import cv2
 import keras
@@ -13,15 +14,15 @@ from util import data_loader
 from util import metrics
 
 RESOLUTION = 128
-CACHE_FILE = './record/weights.53-0.78.hdf5'
+CACHE_FILE = './record/weights.05-0.74.hdf5'
 THRESHOLD = 0.2
 BATCH_SIZE = 64
 
 train_files, val_files = data_loader.get_k_fold_files("baseline.txt", 1, [config.DATA_TYPE_ORIGINAL])
 
-# 取少量数据看模型是否run的起来
-train_files = train_files[:32]
-val_files = val_files[:128]
+# # 取少量数据看模型是否run的起来
+# train_files = train_files[:32]
+# val_files = val_files[:128]
 
 x_train = []
 x_valid = []
@@ -61,19 +62,18 @@ def get_model():
     model.add(BatchNormalization())
     model.add(Dense(13, activation='sigmoid'))
 
-    model.compile(loss='binary_crossentropy',
+    model.compile(loss=metrics.logloss_and_f2score,
                   optimizer='adam',
                   metrics=['accuracy', metrics.smooth_f2_score])
 
     if os.path.isfile(CACHE_FILE):
-        print('####### Loading model from cache ######')
+        print('####### Loading model from cache #######')
         model = load_model(CACHE_FILE, custom_objects={'smooth_f2_score': metrics.smooth_f2_score})
 
     return model
 
 
 def train(model):
-    import time
     start = time.time()
     # 模型可视化，每一次保存会占用几秒钟
     tensorboard = keras.callbacks.TensorBoard(log_dir='./record',
@@ -89,11 +89,11 @@ def train(model):
                                                  period=1)
     model.fit(x_train, y_train,
               batch_size=32,
-              epochs=200,  # Should implement early stopping
+              epochs=10,  # Should implement early stopping
               verbose=1,
               validation_data=(x_valid, y_valid),
               callbacks=[tensorboard, checkpoint])
-    print("####### train model spend %d seconds ######" % (time.time() - start))
+    print("####### train model spend %d seconds #######" % (time.time() - start))
     model.save(CACHE_FILE)
 
 
@@ -101,7 +101,10 @@ def predict(model, files, x, y):
     from sklearn.metrics import fbeta_score
 
     y_pred = model.predict(x, batch_size=128)
+
+    start = time.time()
     best_score, threshold = metrics.best_f2_score(y, y_pred)
+    print("####### search threshold spend %d seconds ######" % (time.time() - start))
 
     print("####### Smooth F2-Score is %f #######" % metrics.smooth_f2_score_np(y, y_pred))
     print("####### F2-Score with threshold 0.2 is %f #######" % fbeta_score(y, (np.array(y_pred) > THRESHOLD).astype(
