6ad6af1e8d19979feda59bb88690693ad0e76938
dinghuanghao <aa531811820@gmail.com>
2018-06-10 00:51:53 +0800

    为图像分割添加异步的图像加载、保存

--- preprocess/segmentation/segmentation.py
+++ preprocess/segmentation/segmentation.py
@@ -1,5 +1,7 @@
 import os
 import time
+import threading
+import queue
 
 import numpy as np
 import skimage.io
@@ -50,7 +52,7 @@ def get_mrcnn_model():
     class InferenceConfig(coco.CocoConfig):
         # 目前仅有一块1050ti在跑
         GPU_COUNT = 1
-        IMAGES_PER_GPU = 1
+        IMAGES_PER_GPU = 10
 
     config = InferenceConfig()
     config.display()
@@ -62,6 +64,74 @@ def get_mrcnn_model():
     model.load_weights(COCO_MODEL_PATH, by_name=True)
 
     return model
+
+
+class ImageSaver(threading.Thread):
+    def __init__(self, class_used, show_image, q: queue.Queue):
+        threading.Thread.__init__(self)
+        self.q = q
+        self.class_used = class_used
+        self.show_image = show_image
+
+    def run(self):
+        print("start image saver thread")
+        while True:
+            results, image_batch, image_save_path_batch = self.q.get()
+            print("get %d results" % len(results))
+            for index in range(len(results)):
+                r = results[index]
+                if self.show_image:
+                    visualize.display_instances(image_batch[index], r['rois'], r['masks'], r['class_ids'],
+                                                CLASS_NAME, self.class_used,
+                                                r['scores'])
+
+                image_masked = image_masking(image_batch[index], r['rois'], r['masks'], r['class_ids'], CLASS_NAME,
+                                             self.class_used)
+
+                if self.show_image:
+                    visualize.display_image(image_masked)
+
+                cv2.imwrite(image_save_path_batch[index], image_masked)
+            self.q.task_done()
+
+
+class ImageLoader(threading.Thread):
+    def __init__(self, image_paths, image_save_paths, q: queue.Queue):
+        threading.Thread.__init__(self)
+        self.q = q
+        self.image_paths = image_paths
+        self.image_save_paths = image_save_paths
+
+    def run(self):
+        print("start image loader thread")
+        mode = "a"
+        if not os.path.exists("segment.log"):
+            mode = "w"
+
+        image_number = len(image_names)
+        image_batch = []
+        image_save_path_batch = []
+
+        for i in range(image_number):
+            if os.path.exists(self.image_save_paths[i]):
+                continue
+            try:
+                image = cv2.imread(self.image_paths[i])
+                image_batch.append(image)
+                image_save_path_batch.append(self.image_save_paths[i])
+                if len(image_batch) == model.config.IMAGES_PER_GPU:
+                    self.q.put((image_batch, image_save_path_batch))
+                    print("put item to queue, queue len=%d" % self.q.qsize())
+                    image_batch = []
+                    image_save_path_batch = []
+
+            except Exception as e:
+                with open("segment.log", mode) as f:
+                    f.write("%s(%s): %s\n" % (time.asctime(time.localtime(time.time())), image_names[i], e))
+
+        if len(image_batch) != 0:
+            print("put last item to queue, queue len=%d" % self.q.qsize())
+            self.q.put((image_batch, image_save_path_batch))
 
 
 def image_segmentation(model, image_dir, image_names: [], save_dir, class_used=("person"), show_image=False):
@@ -83,30 +153,42 @@ def image_segmentation(model, image_dir, image_names: [], save_dir, class_used=(
     image_paths = [os.path.join(image_dir, name) for name in image_names]
     image_save_paths = [os.path.join(save_dir, name) for name in image_names]
 
-    for i in range(len(image_names)):
-        if os.path.exists(image_save_paths[i]):
-            continue
+    image_queue = queue.Queue(maxsize=10)
+    result_queue = queue.Queue()
 
+    image_loader = ImageLoader(image_paths, image_save_paths, image_queue)
+    image_loader.setDaemon(True)
+    image_loader.start()
+
+    image_saver = ImageSaver(class_used, show_image, result_queue)
+    image_saver.setDaemon(True)
+    image_saver.start()
+
+    while True:
         try:
-            image = cv2.imread(image_paths[i])
-            results = model.detect([image], verbose=1)
+            image_batch, image_save_path_batch = image_queue.get()
+            if len(image_batch) < model.config.IMAGES_PER_GPU:
+                class InferenceConfig(coco.CocoConfig):
+                    # 目前仅有一块1050ti在跑
+                    GPU_COUNT = 1
+                    IMAGES_PER_GPU = len(image_batch)
+
+                config = InferenceConfig()
+
+                # 此处之所以要重新建一个模型，是因为之前的模型已经发送到了GPU上，单纯在CPU上修改config没有作用
+                model_1 = modellib.MaskRCNN(mode="inference", model_dir=MODEL_DIR, config=config)
+                model.load_weights(COCO_MODEL_PATH, by_name=True)
+                results = model_1.detect(image_batch, verbose=0)
+                result_queue.put((results, image_batch, image_save_path_batch))
+                break
+            else:
+                results = model.detect(image_batch, verbose=0)
+                result_queue.put((results, image_batch, image_save_path_batch))
         except Exception as e:
             with open("segment.log", mode) as f:
                 f.write("%s(%s): %s\n" % (time.asctime(time.localtime(time.time())), image_names[i], e))
-            continue
 
-        r = results[0]
-
-        if show_image:
-            visualize.display_instances(image, r['rois'], r['masks'], r['class_ids'], CLASS_NAME, class_used,
-                                        r['scores'])
-
-        image_masked = image_masking(image, r['rois'], r['masks'], r['class_ids'], CLASS_NAME, class_used)
-
-        if show_image:
-            visualize.display_image(image_masked)
-
-        cv2.imwrite(image_save_paths[i], image_masked)
+    result_queue.join()
 
 
 def image_masking(image, boxes, masks, class_ids, class_names, class_used):
@@ -146,12 +228,14 @@ def image_masking(image, boxes, masks, class_ids, class_names, class_used):
 
 if __name__ == "__main__":
     model = get_mrcnn_model()
-    image_names = data_loader.list_image_name(path.ORIGINAL_TRAIN_IMAGES_PATH)
-
-    # 将原始数据进行人像分割并保存
-    image_segmentation(model, path.ORIGINAL_TRAIN_IMAGES_PATH, image_names, path.SEGMENTED_TRAIN_IMAGES_PATH,
-                       ['person'], False)
-    # image_names = data_loader.list_image_name(path.ERROR_ORIGINAL_IMAGES_PATH)
+    # image_names = data_loader.list_image_name(path.ORIGINAL_TRAIN_IMAGES_PATH)
     #
     # # 将原始数据进行人像分割并保存
-    # image_segmentation(model, path.ERROR_ORIGINAL_IMAGES_PATH, image_names, path.ERROR_IMAGE_PATH, ['person'], False)
+    # image_segmentation(model, path.ORIGINAL_TRAIN_IMAGES_PATH, image_names, path.SEGMENTED_TRAIN_IMAGES_PATH,
+    #                    ['person'], False)
+    image_names = data_loader.list_image_name(path.ERROR_ORIGINAL_IMAGES_PATH)
+
+    # 将原始数据进行人像分割并保存
+    start = time.time()
+    image_segmentation(model, path.ERROR_ORIGINAL_IMAGES_PATH, image_names, path.ERROR_IMAGE_PATH, ['person'], False)
+    print("#############spend %d seconds" % (time.time() - start))
