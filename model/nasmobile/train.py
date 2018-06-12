7a8bb476908fd586e72c6922fb18885cd87ce1e9
dinghuanghao <aa531811820@gmail.com>
2018-06-12 23:53:21 +0800

    提交三个NasNetMobile模型

---
+++ model/nasmobile/train.py
@@ -0,0 +1,8 @@
+from model.nasmobile import model1_val1
+from model.nasmobile import model2_val1
+from model.nasmobile import model3_val1
+
+
+model1_val1.train()
+model2_val1.train()
+model3_val1.train()
