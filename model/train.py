4ac6da8bfd2b9f799fa33ac58acd977d1443dd8e
dinghuanghao <aa531811820@gmail.com>
2018-06-18 23:22:38 +0800

    新增多个模型使用real transform进行训练

--- model/train.py
+++ model/train.py
@@ -3,9 +3,13 @@ from model.densenet201 import model4_val1
 from model.inceptionresnetv2 import model5_val1
 from model.inceptionv3 import model18_val1
 from model.vgg19 import model4_val1 as vgg4
+from model.resnet50 import model14_val1 as resnet14
+from model.nasmobile import model4_val1 as nas4
 
 model15_val1.train()
 model4_val1.train()
 model5_val1.train()
 model18_val1.train()
 vgg4.train()
+resnet14.train()
+nas4.train()
