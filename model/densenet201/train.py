5d0fd935d3a5dc77be1a456659a690d3a450efb4
dinghuanghao <aa531811820@gmail.com>
2018-06-23 23:31:09 +0800

    尝试分四阶段训练，最后一个阶段打开所有的层

--- model/densenet201/train.py
+++ model/densenet201/train.py
@@ -1,5 +1,7 @@
 from model.densenet201 import model7_val5
 from model.densenet201 import model8_val5
+from model.densenet201 import model9_val5
 
 model7_val5.train()
 model8_val5.train()
+model9_val5.train()
