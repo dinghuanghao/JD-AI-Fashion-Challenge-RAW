78123070f376431a36bcaa8e065dc7362c9ce8fe
dinghuanghao <aa531811820@gmail.com>
2018-06-29 00:31:45 +0800

    新增xception、densnet201针对label3 val2进行优化

--- model/train.py
+++ model/train.py
@@ -1,1 +1,7 @@
 from model.densenet201 import model12_val2
+from model.densenet201 import model7_val2
+from model.xception import model20_val2
+
+model12_val2.train()
+model20_val2.train()
+model7_val2.train()
