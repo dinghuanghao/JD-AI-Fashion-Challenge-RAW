4c1509721f740c2b1898398dded3ed5b086c5d42
dinghuanghao <aa531811820@gmail.com>
2018-06-30 13:20:34 +0800

    在train.py中将根目录引入到sys中

--- model/train.py
+++ model/train.py
@@ -1,7 +1,8 @@
+import os
+import sys
+
+sys.path.append(os.path.abspath("../"))
+
 from model.inceptionv3 import model20_val2
-from model.resnet50 import model15_val2
-from model.densenet201 import model25_val2
-from model.vgg19 import model16_val2
 
-
-model16_val2.train()
+model20_val2.train()
