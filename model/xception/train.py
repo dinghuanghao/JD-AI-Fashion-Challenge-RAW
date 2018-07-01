1dca33c3a6273674bee50aa55a24b60cfeb52156
MathematicianProgrammer <13658247573@163.com>
2018-07-01 18:06:45 +0800

    add model

--- model/xception/train.py
+++ model/xception/train.py
@@ -1,3 +1,10 @@
-from model.xception import model12_val4
+from model.xception import model21_val3
+from model.xception import model21_val4
+from model.xception import model22_val3
+from time import sleep
 
-model12_val4.train()
+model21_val3.train()
+sleep(300)
+model21_val4.train()
+sleep(300)
+model22_val3.train()
