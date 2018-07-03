050dfe754b3f87cfe702763901d3bbccf6970329
dinghuanghao <aa531811820@gmail.com>
2018-07-03 13:51:06 +0800

    resolution 360，xception model25 val1

--- model/xception/train.py
+++ model/xception/train.py
@@ -1,5 +1,5 @@
-from model.xception import model25_val1
+# from model.xception import model25_val1
 from model.xception import model25_val2
 
-model25_val1.train()
+# model25_val1.train()
 model25_val2.train()
