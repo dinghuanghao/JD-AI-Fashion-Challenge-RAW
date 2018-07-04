564b682c11b287b1768eb23a7013055fb670feb7
dinghuanghao <aa531811820@gmail.com>
2018-07-05 00:13:28 +0800

    提交使用大尺寸的inceptionresnetv2，12号标签超过了61%

--- model/inceptionresnetv2/train.py
+++ model/inceptionresnetv2/train.py
@@ -1,5 +1,2 @@
-from model.inceptionresnetv2 import model13_val1
-from model.inceptionresnetv2 import model14_val2
-
-model13_val1.train()
-model14_val2.train()
+from model.inceptionresnetv2 import model15_val5
+model15_val5.train()
