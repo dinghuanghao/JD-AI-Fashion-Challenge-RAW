4b36e8d44a7acdf36106b321e8a0e4871dcbe40f
dinghuanghao <aa531811820@gmail.com>
2018-07-05 17:32:00 +0800

    提交模型

--- model/densenet169/train.py
+++ model/densenet169/train.py
@@ -1,6 +1,3 @@
-from model.densenet169 import model5_val3
-from model.densenet169 import model5_val4
-from time import sleep
-model5_val3.train()
-sleep(300)
-model5_val4.train()
+from model.densenet169 import model5_val1
+
+model5_val1.train()
