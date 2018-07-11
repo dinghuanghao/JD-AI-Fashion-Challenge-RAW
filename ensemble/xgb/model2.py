cf02904357cafcf7fe9259b5bac2ec7c046cb660
dinghuanghao <aa531811820@gmail.com>
2018-07-12 07:49:49 +0800

    重新训练了xgb model 1,2,3,7,9

--- ensemble/xgb/model2.py
+++ ensemble/xgb/model2.py
@@ -5,10 +5,11 @@ from util import ensemble_util
 model = ensemble_util.XGBoostModel(model_path=os.path.abspath(__file__),
                                    corr_threshold=0.9, search=20, top_n=5,
                                    xgb_param={
-                                       'eta': [0.05, 0.1, 0.15, 0.2],
+                                       'eta': [0.1, 0.15, 0.2, 0.25],
                                        'silent': True,  # option for logging
                                        'objective': 'binary:logistic',  # error evaluation for multiclass tasks
-                                       'max_depth': range(3, 11)  # depth of the trees in the boosting process
+                                       'max_depth': range(2, 11),  # depth of the trees in the boosting process
+                                       'min_child_weight': [1, 2, 3, 4, 5]
                                    },
                                    number_round=1000,
                                    )
