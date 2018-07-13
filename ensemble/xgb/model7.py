ec36fce818db271ce21980a8ded1b407c316d999
dinghuanghao <aa531811820@gmail.com>
2018-07-13 20:30:37 +0800

    为AWS虚拟机分配需要训练的模型

--- ensemble/xgb/model7.py
+++ ensemble/xgb/model7.py
@@ -15,4 +15,4 @@ model = ensemble_util.XGBoostModel(model_path=os.path.abspath(__file__),
                                    )
 
 # model.train_all_label()
-model.model_rank(10)
+model.model_rank(20)
--- model/train.py
+++ model/train.py
@@ -3,7 +3,16 @@ import sys
 from time import sleep
 sys.path.append(os.path.abspath("../"))
 
-from model.densenet201 import model32_val3, model32_val4
-model32_val3.train()
-sleep(300)
-model32_val4.train()
+from model.resnet50 import model16_val4 as resnet50_16_4
+from model.vgg16 import model4_val4 as vgg16_4_4
+from model.vgg19 import model17_val4 as vgg19_7_4
+from model.xception import model12_val4 as xception_12_4
+from model.xception import model30_val4 as xception_30_4
+from model.xception import model31_val4 as xception_31_4
+
+resnet50_16_4.train()
+vgg16_4_4.train()
+vgg19_7_4.train()
+xception_12_4.train()
+xception_30_4.train()
+xception_31_4.train()
