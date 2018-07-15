24c1c5596ce5703906dd79056018567384e633f2
dinghuanghao <aa531811820@gmail.com>
2018-07-15 14:52:00 +0800

    提交xgboost top 5 相关模型

--- ensemble/xgb/model51.py
+++ ensemble/xgb/model51.py
@@ -5,7 +5,6 @@ from util import ensemble_util
 model = ensemble_util.XGBoostModel(model_path=os.path.abspath(__file__),
                                    corr_threshold=0.9, search=20, top_n=5,
                                    eval_func=ensemble_util.xgb_sparse_greedy_f2_metric,
-                                   meta_model_dir="E:\\backup\\jdfc",
                                    xgb_param={
                                        'eta': [0.05],
                                        'silent': True,  # option for logging
@@ -17,6 +16,3 @@ model = ensemble_util.XGBoostModel(model_path=os.path.abspath(__file__),
                                    )
 
 model.train_all_label()
-# model.model_rank(10)
-# model.get_meta_predict([1, 2, 3, 4, 5], False)
-# model.find_segmented_model()
--- ensemble/xgb/model52.py
+++ ensemble/xgb/model52.py
@@ -5,9 +5,8 @@ from util import ensemble_util
 model = ensemble_util.XGBoostModel(model_path=os.path.abspath(__file__),
                                    corr_threshold=0.9, search=20, top_n=5,
                                    eval_func=ensemble_util.xgb_sparse_greedy_f2_metric,
-                                   meta_model_dir="E:\\backup\\jdfc",
                                    xgb_param={
-                                       'eta': [0.05],
+                                       'eta': [0.1],
                                        'silent': True,  # option for logging
                                        'objective': 'binary:logistic',  # error evaluation for multiclass tasks
                                        'max_depth': range(2, 11),  # depth of the trees in the boosting process
@@ -17,6 +16,4 @@ model = ensemble_util.XGBoostModel(model_path=os.path.abspath(__file__),
                                    )
 
 model.train_all_label()
-# model.model_rank(10)
-# model.get_meta_predict([1, 2, 3, 4, 5], False)
-# model.find_segmented_model()
+
