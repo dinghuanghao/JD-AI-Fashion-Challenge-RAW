f19c8d222186ed81693bb7dbd5c9ad45b64dae3f
dinghuanghao <aa531811820@gmail.com>
2018-07-15 10:32:34 +0800

    提交模型

--- ensemble/xgb/model5.py
+++ ensemble/xgb/model5.py
@@ -16,7 +16,24 @@ model = ensemble_util.XGBoostModel(model_path=os.path.abspath(__file__),
                                    number_round=1000,
                                    )
 
-model.train_all_label()
+# model.train_all_label()
 # model.model_rank(10)
 # model.get_meta_predict([1, 2, 3, 4, 5], False)
 # model.find_segmented_model()
+
+data_x, data_y = model.build_all_datasets()
+print(data_x.shape)
+print(data_y[:4, :])
+pre_y = model.predict_real(data_x)
+print(pre_y[:4, :])
+
+print(data_y.shape)
+print(pre_y.shape)
+from sklearn.metrics import fbeta_score
+f2_marco = 0
+for i in range(13):
+    f2 = fbeta_score(data_y[:, i], pre_y[:, i], beta=2)
+    f2_marco += f2
+    print(f2)
+f2_marco /= 13
+print("average f2 is %6f" % f2_marco)
