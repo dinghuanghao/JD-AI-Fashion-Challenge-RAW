e100614184f91d6cb6bd20913c20cfd7c13966fa
dinghuanghao <aa531811820@gmail.com>
2018-07-15 11:37:39 +0800

    重新统计

--- ensemble/xgb/model3.py
+++ ensemble/xgb/model3.py
@@ -19,7 +19,7 @@ model = ensemble_util.XGBoostModel(model_path=os.path.abspath(__file__),
 
 # model.train_all_label()
 # model.model_rank(10)
-model.get_meta_predict([1, 2, 4, 5], False)
+# model.get_meta_predict([1, 2, 4, 5], False)
 # model.find_segmented_model()
 # model.model_merge(["model19", "model21"])
 
