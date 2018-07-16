ae99869914e5fd730b2e7c53aefeabc9bee127eb
MathematicianProgrammer <13658247573@163.com>
2018-07-16 18:50:02 +0800

    add retrain model and xgboost

--- model/train.py
+++ model/train.py
@@ -3,5 +3,12 @@ import sys
 from time import sleep
 sys.path.append(os.path.abspath("../"))
 
-from model.inceptionresnetv2 import model16_val4
-model16_val4.train()
+# from model.inceptionresnetv2 import model16_val4
+# model16_val4.train()
+
+# from model.inceptionv3 import model21_val3, model23_val3
+# model21_val3.train()
+# model23_val3.train()
+
+from model.resnet50 import model16_val3
+model16_val3.train()
