90c1d2668c5da497dbba5e5840897b8a222692db
dinghuanghao <aa531811820@gmail.com>
2018-07-17 13:43:49 +0800

    新增环境变化

--- ensemble/xgb/model153.py
+++ ensemble/xgb/model153.py
@@ -1,5 +1,7 @@
 import os
-
+import sys
+sys.path.append(os.path.abspath("../"))
+sys.path.append(os.path.abspath("../../"))
 from util import ensemble_util
 
 model = ensemble_util.XGBoostModel(model_path=os.path.abspath(__file__),
