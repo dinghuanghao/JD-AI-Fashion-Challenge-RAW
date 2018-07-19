5fbc8b1b248b9bf417bb49cf97658d53aeb360a0
dinghuanghao <aa531811820@gmail.com>
2018-07-19 08:26:13 +0800

    [cnn][inceptionv3]: 去掉fc layer，并一开始就训练所有层，效果不错

--- model/inceptionv3/train.py
+++ model/inceptionv3/train.py
@@ -1,13 +1,3 @@
-from model.inceptionv3 import model21_val3
-from model.inceptionv3 import model21_val4
-from model.inceptionv3 import model22_val3
-from model.inceptionv3 import model23_val3
-from time import sleep
+from model.inceptionv3 import model26_val3
 
-model21_val3.train()
-sleep(300)
-model21_val4.train()
-sleep(300)
-model22_val3.train()
-sleep(300)
-model23_val3.train()
+model26_val3.train()
