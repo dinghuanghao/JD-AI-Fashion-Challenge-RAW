b790c2d1d6b2f0f108b07378de70538647d737c5
MathematicianProgrammer <13658247573@163.com>
2018-08-23 21:40:25 +0800

    add test model

--- model/train.py
+++ model/train.py
@@ -3,14 +3,11 @@ import sys
 from time import sleep
 sys.path.append(os.path.abspath("../"))
 
-# from model.xception import model29_val2
-# from model.xception import model29_val3, model29_val4, model30_val2, model30_val5
-#
-# model29_val2.train()
-# model29_val3.train()
-# model29_val4.train()
-# model30_val5.train()
-# model30_val2.train()
+from model.densenet169 import model111_val1
+model111_val1.train()
 
-from model.inceptionresnetv2 import  model17_val3
-model17_val3.train()
+from model.resnet50 import model111_val1
+model111_val1.train()
+
+from model.xception import model111_val1
+model111_val1.train()
