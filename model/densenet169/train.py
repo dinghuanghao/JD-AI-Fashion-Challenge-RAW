3ee228c4c1c7dcb80a8b02b771476eabe94b6c0f
MathematicianProgrammer <13658247573@163.com>
2018-09-05 20:45:07 +0800

    add test for val2 desenet169

--- model/densenet169/train.py
+++ model/densenet169/train.py
@@ -1,18 +1,17 @@
-from model.densenet169 import model100_val2, model101_val2, model102_val2, model103_val2, model104_val2, model105_val2
-model100_val2.train()
-model101_val2.train()
-model102_val2.train()
-model103_val2.train()
-model104_val2.train()
-model105_val2.train()
-from model.densenet169 import model106_val2, model107_val2, model108_val2, model109_val2, model110_val2, model111_val2
-model106_val2.train()
-model107_val2.train()
-model108_val2.train()
-model109_val2.train()
-model110_val2.train()
-model111_val2.train()
-from model.densenet169 import model112_val2, model113_val2, model114_val2
+# from model.densenet169 import model104_val2, model105_val2
+# model100_val2.train()
+# model101_val2.train()
+# model102_val2.train()
+# model103_val2.train()
+# model104_val2.train()
+# model105_val2.train()
+# from model.densenet169 import model108_val2, model109_val2, model110_val2, model111_val2
+# model106_val2.train()
+# model107_val2.train()
+# model108_val2.train()
+# model109_val2.train()
+# model110_val2.train()
+# model111_val2.train()
+from model.densenet169 import model112_val2, model113_val2
 model112_val2.train()
 model113_val2.train()
-model114_val2.train()
