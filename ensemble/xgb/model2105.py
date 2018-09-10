a9ea2d7d91af2e49945d8504ce1afbbca0f04396
dinghuanghao <aa531811820@gmail.com>
2018-09-10 09:20:13 +0800

    add xgb 2101 2102 2103 2104 2105

---
+++ ensemble/xgb/model2105.py
@@ -0,0 +1,24 @@
+import os
+import sys
+
+sys.path.append(os.path.abspath("../"))
+sys.path.append(os.path.abspath("../../"))
+from util import ensemble_util
+
+model = ensemble_util.XGBoostModel(model_path=os.path.abspath(__file__),
+                                   corr_threshold=0.9, search=15, top_n=10,
+                                   eval_func=ensemble_util.xgb_sparse_greedy_f2_metric,
+                                   meta_model_dir="D:\\github\\JDC\\competition",
+                                   xgb_param={
+                                       'eta': [0.2],
+                                       'silent': True,  # option for logging
+                                       'objective': 'binary:logistic',  # error evaluation for multiclass tasks
+                                       'max_depth': range(2, 11),  # depth of the trees in the boosting process
+                                       'min_child_weight': [1, 2, 3, 4, 5]
+                                   },
+                                   number_round=1000,
+                                   )
+
+model.get_meta_predict([1, 2, 3, 4, 5], True)
+# model.model_merge(["model1201", "model1202", "model1203", "model1204"])
+# model.build_and_predict_test()
